ca1fed1b7d3a8a63a863c7dfa09d5a3ef6ebeed5
stoliarovea <stoliarovea@gmail.com>
2023-07-24 23:22:38 +0300

    24.07.2023-3

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -5,20 +5,25 @@ import MoviesCard from '../MoviesCard/MoviesCard';
 function MoviesCardList() {
 
     return (
-        <div className="movies-card-list">
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
-            <MoviesCard />
-        </div>
+        <>
+            <div className="movies-card-list">
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+                <MoviesCard />
+            </div>
+            <div className="movies-card-list__button-container">
+                <button className="movies-card-list__button">Еще</button>
+            </div>
+        </>
     );
 }
 
