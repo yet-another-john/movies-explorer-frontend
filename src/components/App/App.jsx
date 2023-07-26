b12233a057877b7202614b64f9495ec351f82ae3
stoliarovea <stoliarovea@gmail.com>
2023-07-27 00:01:58 +0300

    26.07.2023-4

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,51 +1,61 @@
 import './App.css';
 import React from 'react';
-import NotFound from '../NotFound/NotFound';
+import { Routes, Route } from 'react-router-dom';
+import Register from '../Register/Register';
+import Login from '../Login/Login';
+import Header from '../Header/Header';
+import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
 
 function App() {
 
   return (
     <div className="app-page">
       <div className="app-content">
-        <NotFound />
+        <Routes>
+          <Route path="/signup" element={
+            <>
+              <Register />
+            </>
+          } />
+          <Route path="/signin" element={
+            <>
+              <Login />
+            </>
+          } />
+          <Route path="/" element={
+            <>
+              <Header />
+              <Main />
+              <Footer />
+            </>
+          } />
+          <Route path="/movies" element={
+            <>
+              <Header />
+              <Movies />
+              <Footer />
+            </>
+          } />
+          <Route path="/saved-movies" element={
+            <>
+              <Header />
+              <SavedMovies />
+              <Footer />
+            </>
+          } />
+          <Route path="/profile" element={
+            <>
+              <Profile />
+            </>
+          } />
+        </Routes>
       </div>
     </div>
   );
 }
 
 export default App;
-
-/*
-        <Routes>
-          <Route path="/signup" element={
-            <>
-
-            </>
-          } />
-          <Route path="/signin" element={
-            <>
-
-            </>
-          } />
-          <Route path="/" element={
-            <>
-
-            </>
-          } />
-          <Route path="/movies" element={
-            <>
-
-            </>
-          } />
-          <Route path="/saved-movies" element={
-            <>
-
-            </>
-          } />
-          <Route path="/profile" element={
-            <>
-
-            </>
-          } />
-        </Routes>
-*/
